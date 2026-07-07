13172ad6968ac25d61ec4101d22fa7dda990b0c3
Arpita <arpitalakkhiwal3@gmail.com>
2026-07-07 21:02:06 +0530

    Initial QRCS Project

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import About from "../Components/About";
+import Features from "../Components/Features";
+import Footer from "../Components/Footer";
+
+function Home({ setPage, navbarOnly = false }) {
+  return (
+    <>
+      <Navbar setPage={setPage} />
+      {!navbarOnly && (
+        <>
+          <Hero />
+          <Footer />
+        </>
+      )}
+    </>
+  );
+}
+export default Home;
